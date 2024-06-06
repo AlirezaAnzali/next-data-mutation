32b847daad76b694be4fa254d509d486a8be0e41
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2024-06-06 15:17:17 -0400

    Configuring Static and Dynamic Page Metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,19 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: 'Browse all our X posts',
+//   description: 'Browse and share amazing posts.',
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+
+  return {
+    title: `Browse all our ${posts.length} posts`,
+    description: "Browse and share amazing posts.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
